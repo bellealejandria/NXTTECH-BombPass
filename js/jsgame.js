f5e5f9cb120c46728bca010f6bd16125868cd5c3
Miguel Sietereales <miguel.sietereales@gmail.com>
2017-08-18 03:42:39 +0800

    Added "things to do" as comments

--- js/jsgame.js
+++ js/jsgame.js
@@ -1,3 +1,11 @@
+//FIREBASE
+//STATUS 2 IF DEAD
+//STATUS 1 IF HAWAK BOMB
+//STATUS 0 WAITING
+
+//READY 1 IF NAKAPASOK NA
+//READ 0 IF DI PA
+
 /*// Initialize Firebase
 var config = {
     apiKey: "AIzaSyDggdwXC75QRKKZClRa7K2kQKrpXOhZuoQ",
@@ -43,6 +51,8 @@ function addPlayers(){
   roomRef.once("value", function(snapshot){
     var players = snapshot.child("3967A").val();
 
+    ////CHANGE CODE DUN GAWIN NA KUNG ANO UNG NA GENERATE NA GAME CODE(?) KANINA////
+
     Object.keys(players).forEach(function(key) {
       if(snapshot.child("3967A").child(key).child("name").val() != ""){
         console.log(snapshot.child("3967A").child(key).child("name").val());
@@ -63,11 +73,16 @@ function addPlayers(){
       $("#playername-" + (ctr + 1)).html(playerName[ctr]);
     }
 
+    //CAN REMOVE PERO WAG MUNA//
     //Removes unecessary buttons from players
     var playerBtnCount = playerCount - 1
     for(ctr = 3; ctr > playerBtnCount; ctr--){
       $("#playerbtn" + ctr).hide();
     }
+    //////////////////////////
+
+    ////////////INSERT CODE IF READY LAHAT THEN START GAME////////////////
+    //NO IDEA PANO//
 
     startGame();
   });
@@ -80,6 +95,9 @@ function bombExplode(){
   if($("div").hasClass("it")){
     var tempo = $(".it").attr("id");
     playerDead.push(tempo);
+
+    //////////////INSERT CODE UPDATE STATUS OF THE PLAYER HOLDING THE BOMB TO '2'/DEAD////////////
+    //SINCE NAKUKUHA NAME PWEDE COMPARE SA FIREBASE UNG VALUE NG NAME SA NAME NG MAY HAWAK NA BOMB THEN CHANGE STATUS TO '2' OR DEAD//
 
     for(ctr = 0; ctr < playerAlive.length; ctr++){
       if(playerAlive[ctr] == tempo){
@@ -130,6 +148,9 @@ function randomizeBomb(){
     if(playerAlive.length != 1){
       it = Math.floor((Math.random() * playerAlive.length));
 
+      //SET STATUS OF THE PLAYER TO 1 SINCE HAWAK UNG BOMB
+      //SINCE NAKUKUHA NAME PWEDE COMPARE SA FIREBASE UNG VALUE NG NAME SA NAME NG MAY HAWAK NA BOMB THEN CHANGE STATUS TO '1' OR BASTA MAY HAWAK BOMB//
+
       $("#" + playerAlive[it]).addClass("active it");
       $("#bmb" + playerAlive[it]).html('<img class="img-responsive col-lg-8 col-lg-offset-2" src="assets/img/bomb.png"/>');  
     }     
@@ -154,4 +175,13 @@ window.onload = function() {
   console.log(name)
 
 };
-	
+
+//FOR THIS WEB DESKTOP//
+//ADD ON UPDATE CHECK MO LANG KUNG NAKANINO UNG BOMB GAMIT UNG STATUS LAGI TAPOS GAYAHIN UNG CODE SA RANDOMIZE BOMB PARA ALAM PANO MO LILIAPT DESIGN DUN SA MAY HAWAK NA BOMB TALAGA//
+
+
+//FOR MOBILE//
+//HIWALAY NA FILE NALANG AND DIFFERENT LINK//
+//YUNG SA MOBILE MADALI NALANG BASTA MA-KUHA UNG CODE AND MAKUHA UNG NAME TAPOS MA IAYOS TO//
+//ONCLICK MAG WORK LANG IF THIS NAME HAS A STATUS OF 1 OR HOLDING THE BOMB//
+//SET THE NUMBER OF BUTTONS BASED ON THE NUMBER OF PLAYERS NA BUHAY PA//
